e1e9afe769704111314d3f909d550acb9599d4a5
liulonghua <llh@kandaovr.com>
2018-07-10 17:07:50 +0800

    add function

--- auth/crypto.go
+++ auth/crypto.go
@@ -1,9 +1,46 @@
 package auth
 
+import (
+	"time"
+    "../conf"
+	"bytes"
+	"encoding/binary"
+	"math/rand"
+	"crypto/sha256"
+	"strconv"
+)
 
-func GetRandomString(length int8) string {
+
+func RandomChoice(choicechars string) string{
+	length := len(choicechars)
+	random := rand.Intn(length)
+	return choicechars[random:random+1]
+}
+
+
+func GetRandomString(length int) string {
 	AllowedChars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
-	
-	return AllowedChars
+	var seek int32
+	t := time.Now()
+	timesamp := t.UnixNano()
+	buf := bytes.Buffer{}
+	buf.WriteString(AllowedChars)
+	buf.WriteString(strconv.FormatInt(timesamp,10))
+	buf.WriteString(conf.SECRETKEY)
+	s := buf.String()
+	haser := sha256.New()
+	haser.Write([]byte(s))
+	shash := haser.Sum(nil)
+	bytesBuffer := bytes.NewBuffer(shash)
+
+	binary.Read(bytesBuffer, binary.BigEndian, &seek)
+    rand.Seed(int64(seek))
+
+	buf2 := bytes.Buffer{}
+	for i:=0;i<length;i++{
+		tmp := RandomChoice(AllowedChars)
+		buf2.WriteString(tmp)
+	}
+	return buf.String()
 }
