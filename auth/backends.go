98ecdc5647cac5c6553ebbb01462766f61dc9bcc
liulonghua <llh@kandaovr.com>
2018-07-12 18:04:09 +0800

    change

--- auth/backends.go
+++ auth/backends.go
@@ -28,3 +28,17 @@ func Authenticate(username string ,password string) *User {
 	 }
 	 return nil
 }
+
+
+func get_user_permissions(user User) []*Permission {
+	o := orm.NewOrm()
+	var p []*Permission
+	_,ok := o.QueryTable("auth_permission").Filter("Users__User__Id",user.Id).All(&p)
+	if ok == nil{
+		return p
+	}
+	return nil
+}
+
+
+//func get_group_permissions()
